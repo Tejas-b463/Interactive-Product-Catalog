001f8bc8ab2b5ebd54f0e7df9d9b80d320b2effa
Tejas-b463 <108131310+Tejas-b463@users.noreply.github.com>
2024-02-14 11:59:09 +0530

    Sign in and sign Up form validation

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,22 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "../styles/SignIn.css";
+import { checkValidData } from "../utils/validate";
 
 const SignIn = () => {
   const [isSignIn, setIsSignIn] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const toggleSignInForm = () => {
     setIsSignIn(!isSignIn);
   };
 
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
+  };
+
   return (
     <>
       <div className="form-group">
-        <form action="">
+        <form onSubmit={(e) => e.preventDefault()}>
           <h1>{isSignIn ? "Sign In" : "Sign Up"}</h1>
-          <input type="text" placeholder="Username" />
-          {!isSignIn && <input type="text" placeholder="Name" />}
-          <input type="password" placeholder="Password" />
-          <button>{isSignIn ? "Sign In" : "Sign Up"}</button>
+          {!isSignIn && <input type="text" placeholder="Name" required />}
+          <input ref={email} type="text" placeholder="Email" />
+
+          <input ref={password} type="password" placeholder="Password" />
+          <p style={{ color: "red" }}>{errorMessage}</p>
+          <button onClick={handleButtonClick}>
+            {isSignIn ? "Sign In" : "Sign Up"}
+          </button>
           <p onClick={toggleSignInForm}>
             {isSignIn
               ? "New User? Create Account"
